d5741e8924ca181ce8002b70db043fbc7f3d44fa
rebalkin <reuvenb87@gmail.com>
2025-09-19 23:09:59 +0000

    updated option pricing

---
+++ src/.ipynb_checkpoints/monte_carlo-checkpoint.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+# Single-step geometric Brownian motion
+
+def GBM_single(S0, mu, sigma, dt):
+    """
+    Simulate a single step of geometric Brownian motion.
+
+    Parameters:
+        S0 (float): Initial asset price
+        mu (float): Expected return (drift)
+        sigma (float): Volatility
+        dt (float): Time increment
+
+    Returns:
+        float: Simulated asset price after one time step
+    """
+    Z = np.random.normal()
+    S1 = S0 * np.exp((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * Z)
+    return S1
+
+def GBM_time_series(S0, mu, sigma, T,dt):
+    n = int(T/dt)
+    prices = [S0]
+    for i in range(n):
+        t = i*dt
+        drift = mu(t) if callable(mu) else mu
+        vol = sigma(t) if callable(sigma) else sigma
+        S_init = prices[-1]
+        S_next = GBM_single(S_init, drift, vol, dt)
+        prices.append(S_next)
+    return np.array(prices)
+
+def GBM_time_series_fast(S0, mu, sigma, T,dt,m):
+    n = int(round(T / dt))
+    logpaths = np.random.normal(size=(n,m))
+    times = np.arange(n) * dt  
+    sig = sigma(times) if callable(sigma) else sigma
+    muu = mu(times)     if callable(mu)     else mu
+    sig, muu = np.asarray(sig), np.asarray(muu)
+    logpaths *= sig * np.sqrt(dt)
+    logpaths += (muu - 0.5 * sig**2)*dt
+    prices = S0*np.exp(np.cumsum(logpaths,axis=0))
+
+    stocks = np.empty((n+1, m))
+    stocks[0, :] = S0
+    stocks[1:, :] =prices
+
+    time_grid = np.linspace(0, T, n+1)
+    return time_grid,stocks
+
+# def GBM_time_series_fast(S0, mu, sigma, T,dt):
+#     n = int(round(T / dt))
+#     paths = np.random.normal(size=n)
+#     times = np.arange(n) * dt  
+#     sig = sigma(times) if callable(sigma) else sigma
+#     muu = mu(times)     if callable(mu)     else mu
+#     sig, muu = np.asarray(sig), np.asarray(muu)
+#     paths *= sig * np.sqrt(dt)
+#     paths += (muu - 0.5 * sig**2)*dt
+
+#     stocks = np.concatenate([[S0],S0*np.exp(np.cumsum(paths))])
+#     return stocks
+
+def MC_price_fast(S0, r, sigma, T,dt,payoff,N):
+    if not callable(payoff):
+        print("Error: payoff needs to ba callable")
+        return None
+
+    timegrid, paths = GBM_time_series_fast(1, r, sigma, T,dt,N)
+    payoffs = payoff(np.outer(S0, paths[-1,:]))
+    mean = payoffs.mean(axis = 1)              # or np.mean(arr)
+    std = payoffs.std(axis = 1,ddof=1)
+
+    if not callable(r):
+        disc = np.exp(-r*T)
+    else:
+        tgrid = np.arange(0, T+dt, dt)
+        integral_r = np.trapzoid(r(tgrid), dx=dt)
+        disc = np.exp(-integral_r)
+    
+    price = disc * mean
+    stderr = disc * std/np.sqrt(N)
+    if np.ndim(S0) == 0:
+        return float(price), float(stderr)
+    return price, stderr
+
+
+def MC_price(S0, r, sigma, T,dt,payoff,N):
+    if not callable(payoff):
+        print("Error: payoff needs to ba callable")
+        return None
+    if not callable(r) and not callable(sigma):
+        return MC_price_consts(S0, r, sigma, T,payoff,N)
+    res=[]
+    for i in range(N):
+        S = GBM_time_series(S0, r, sigma, T,dt)
+        res.append(payoff(S[-1]))
+    mean = res.mean()              # or np.mean(arr)
+    std = res.std(ddof=1)    
+    if not callable(r):
+        result = np.exp(-r*T)*mean
+        error = std/np.sqrt(N)
+        return result, error
+    else:
+        # r is a function of time
+        integral_r = np.trapzoid([r(t) for t in np.arange(0,T+dt,dt)], dx=dt)
+        result = np.exp(-integral_r)*mean
+        error = std/np.sqrt(N)
+        return result, error
+    
+    
+def MC_price_consts(S0, r, sigma, T,payoff,N):
+    # This can be used the validate Black-Scholes 
+    if not callable(payoff):
+        print("Error: payoff needs to ba callable")
+        return None
+    res=[]
+    for i in range(N):
+        S = GBM_single(S0, r, sigma, T)
+        res.append(np.exp(-r*T)*payoff(S))
+    res = np.array(res)
+
+    mean = res.mean()              # or np.mean(arr)
+    std = res.std(ddof=1)    
+
+    return mean, std/np.sqrt(N)
+        
+
+
+    
