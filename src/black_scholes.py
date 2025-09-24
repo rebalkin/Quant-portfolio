25514ccf7372c286765bf3144b0a087642840ec7
rebalkin <reuvenb87@gmail.com>
2025-09-24 19:50:19 +0000

    new file:   notebooks/vol_arb.ipynb 	modified:   src/black_scholes.py

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -41,3 +41,27 @@ def implied_vol(S, E, r, D, T, option_type,price):
         return brentq(objective, 1e-6, 2)  # search between 0.000001 and 500%
     except ValueError:
         return np.nan
+    
+
+def delta(S, E, r, D,sigma, T, option_type):
+    d1 = d1_func(S, E, r, D,sigma, T)
+    if option_type=="call":
+        return np.exp(-D*T)*N(d1)
+    elif option_type=="put":
+        return np.exp(-D*T)*(N(d1)-1)
+    elif option_type=="digital_call":
+        return np.exp(-D*T)*Np(d1)/(S*sigma*np.sqrt(T))
+    elif option_type=="digital_put":
+        return -np.exp(-D*T)*Np(d1)/(S*sigma*np.sqrt(T))
+    else:
+        raise ValueError("option_type must be either 'call', 'put', 'digital_call' or 'digital_put'")
+    
+def gamma(S, E, r, D,sigma, T, option_type):
+    d1 = d1_func(S, E, r, D,sigma, T)
+    if option_type in ["call","put"]:
+        return np.exp(-D*T)*Np(d1)/(S*sigma*np.sqrt(T))
+    elif option_type in ["digital_call","digital_put"]:
+        return -np.exp(-D*T)*d1*Np(d1)/(S**2*sigma**2*T)
+    else:
+        raise ValueError("option_type must be either 'call', 'put', 'digital_call' or 'digital_put'")
+    
